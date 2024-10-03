e30a9d37ac25291ad9f8f00343e119ba10890529
Brickish <chuen1d2@gmail.com>
2024-10-03 19:30:52 +0800

    ReadyBasicHTML

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello")
+from flask import Flask, render_template, redirect, url_for, request, flash, session, make_response
+import re, json, hashlib, os
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+app.permanent_session_lifetime = timedelta(seconds=60)
+@app.route("/")
+def homepage():
+    return render_template ("homepage.html",user = "")
